189ff9485d9203389f372b5022145c1193b24dc1
SauloJuniorsj <juniorroseiro@hotmail.com>
2021-03-30 21:44:50 -0300

    REST aula implementado

--- app/Http/Controllers/AulaController.php
+++ app/Http/Controllers/AulaController.php
@@ -2,20 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class AulaController extends Controller{
     public function show($id = null){
-        echo 'Retorno do método ';
+        if($id){
+            $aulas = \App\Models\Aula::find($id);
+        }else{
+            $aulas = \App\Models\Aula::all();
+        }
+        return response($aulas, 200, [
+            'Content-Type' => 'application/json'
+        ]);
     }
 
-    public function create(){
-        echo 'Cria um recurso';
+    public function create(Request $request){
+        if(isset($request->nome)){
+            $aula = new \App\Models\Aula();
+            $aula->nome = $request->nome;
+            $aula->save();
+            return response($aula, 201, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        return response([
+            'error' => 'nome nao foi informado!'
+        ], 404, [
+            'Content-Type' => 'application/json'
+        ]);
     }
 
-    public function update($id){
-        echo 'update o id : ' . $id;
+    public function update(Request $request, $id){
+        if(isset($request->nome) && $id){
+            $aula = \App\Models\Aula::find($id);
+            $aula->nome = $request->nome;
+            $aula->save();
+            return response($aula, 200, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        return response([
+            'error' => 'Nome da aula ou id nao foi informado!'
+        ], 404, [
+            'Content-Type' => 'application/json'
+        ]);
     }
 
     public function destroy($id){
-        echo 'destuir o id : ' . $id;
+        if($id){
+            $aula = \App\Models\Aula::find($id);
+            $aula->delete();
+            return response($aula, 200, [               //resposta true, retorna 1, $aula retorna o objeto deletado
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        return response([
+            'error' => 'id nao foi informado!'
+        ], 404, [
+            'Content-Type' => 'application/json'
+        ]);
     }
 }
